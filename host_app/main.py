fc22066b1f31e2b8644701bc17410bfd4a665bc5
Andrei Ionut Damian <andrei.damian@me.com>
2025-03-06 08:41:44 +0200

    fix: several tryouts

--- host_app/main.py
+++ host_app/main.py
@@ -2,20 +2,43 @@ import subprocess
 
 def log(*args):
   print(*args, flush=True)
+
+
+def run_command(cmd: list) -> str:
+  log("Running command:", cmd)
+  try:
+    res = subprocess.run(cmd, capture_output=True, text=True)
+    result = res.stdout
+    return_code = res.returncode
+    log(f"Return: {return_code}, Errors: {res.stderr}")
+  except Exception as e:
+    log(f"Error: {e}")
+    result = ""
+  if return_code != 0:
+    result = ""
+  return result
   
   
 def runner() -> str:
   CMD = ["docker", "run", "aidamian/test-did-external-app"]
   log("Running the child app...")
-  try:
-    res = subprocess.run(CMD, capture_output=True, text=True)
-  except Exception as e:
-    log(f"Error: {e}")
-    return ""
+  result = run_command(CMD)
   log("Child app finished.")
-  return res.stdout
+  return result
+
+def check_docker():
+  result = True
+  res1 = run_command(["docker", "version"])
+  res2 = run_command(["docker", "ps"])
+  if res1 == "" or res2 == "":
+    result = False
+  return result
 
 def main():
+  log("Checking Docker...")
+  if not check_docker():
+    log("Docker is not running.")
+    return
   res = runner()
   log("Result:", res)
   log("Done.")
