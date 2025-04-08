edfaa0f61211b2ff23d776629ebcdfe11f4af9ce
Andrei Damian <andrei.damian@me.com>
2025-04-08 18:01:19 +0300

    fix: looper

--- simple_looper/main.py
+++ simple_looper/main.py
@@ -2,20 +2,38 @@ import time
 import os
 import datetime
 import signal
+import psutil
 
 class Looper(object):
   def __init__(self):
     self.done = False
     return
+  
+  def get_cpu_limit(self):
+    try:
+      cpu_quota = int(open('/sys/fs/cgroup/cpu/cpu.cfs_quota_us').read())
+      cpu_period = int(open('/sys/fs/cgroup/cpu/cpu.cfs_period_us').read())
+      return cpu_quota / cpu_period if cpu_quota > 0 else os.cpu_count()
+    except Exception:
+      return os.cpu_count()
 
+  def get_memory_limit(self):
+    try:
+      mem_limit = int(open('/sys/fs/cgroup/memory/memory.limit_in_bytes').read())
+      return mem_limit / (1024 * 1024)
+    except Exception:
+      return psutil.virtual_memory().total / (1024 * 1024)  
+  
   def run(self):
     hostname = os.getenv("HOSTNAME", "UNKNOWN")
     self.cnt = 0
     print(f"Starting looper on {hostname}", flush=True)
     while not self.done:
       self.cnt += 1
+      mem_mb = self.get_memory_limit()
+      cpus = self.get_cpu_limit()
       str_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-      print(f"[{str_date}][{hostname}]: Iter #{self.cnt}", flush=True)
+      print(f"[{str_date}][{hostname}]: Iter #{self.cnt}, Cpus: {cpus}, Mem: {mem_mb:,.0f} MiB", flush=True)
       time.sleep(5)
     return
 
@@ -31,3 +49,4 @@ if __name__ == "__main__":
   signal.signal(signal.SIGINT, signal_handler)
 
   eng.run()
+  print("Done.", flush=True)
---
+++ simple_looper/test.sh
@@ -0,0 +1,2 @@
+./build.sh
+docker run --memory 512m --cpus 2 aidamian/simple_looper
